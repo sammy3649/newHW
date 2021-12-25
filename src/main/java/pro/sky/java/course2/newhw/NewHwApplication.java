df233a871fd9552ce6aad11a9720fc5722d61451
Samantha <sammy23051996@gmail.com>
2021-12-25 13:53:38 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/newhw/NewHwApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.newhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/newhw/NewHwApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.newhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
